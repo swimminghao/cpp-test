9a2ad2e7c96cefb4e729ef0adf0f43a9bddda0bc
xh <xuehao@shanzhen.com>
2021-08-09 22:42:57 +0800

    test

--- Solution.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by xh on 2021/2/21.
-//
-
-#include "Solution.h"
--- cmake-build-debug/CMakeFiles/first.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/first.dir/DependInfo.cmake
@@ -1,16 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/xh/workspaces/clion/first/Solution.cpp" "/Users/xh/workspaces/clion/first/cmake-build-debug/CMakeFiles/first.dir/Solution.cpp.o"
-  "/Users/xh/workspaces/clion/first/main.cpp" "/Users/xh/workspaces/clion/first/cmake-build-debug/CMakeFiles/first.dir/main.cpp.o"
+  "/Users/xh/workspaces/clion/Solution.cpp" "/Users/xh/workspaces/clion/cmake-build-debug/CMakeFiles/first.dir/Solution.cpp.o"
+  "/Users/xh/workspaces/clion/main.cpp" "/Users/xh/workspaces/clion/cmake-build-debug/CMakeFiles/first.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
