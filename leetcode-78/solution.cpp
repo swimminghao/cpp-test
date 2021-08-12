8848519a34a8d6df83083f67f2770b2fd424d977
xh <xuehao@wayyue.com>
2021-08-12 11:28:26 +0800

    leetcode-78 回溯法

--- leetcode-78/solution.cpp
+++ leetcode-78/solution.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "solution.h"
+#include "solution1.h"
 int main() {
     solution s;
+    solution1 s1;
 //    vector<int> a = {};
     vector<int> a = {1,2,3};
+    vector<int> a1 = {1,2,3};
     vector<vector<int>> b = s.subsets(a);
+    vector<vector<int>> b1 = s1.subsets(a1);
     std::cout << "Hello, World!\n";
 return 0;
 }
