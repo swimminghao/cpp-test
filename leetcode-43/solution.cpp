1eb9a4ecd9baf9692a155f929c7514a46382d48b
xh <xuehao@wayyue.com>
2021-08-20 16:13:22 +0800

    字符串相乘 commit

---
+++ leetcode-43/solution.cpp
@@ -0,0 +1,18 @@
+//
+// Created by xh on 2021/8/20.
+//
+
+#include "solution.h"
+#include <iostream>
+#include "solution.h"
+
+int main() {
+    solution s;
+
+//    vector<int> a = {};
+//    vector<int> a = {1,2,3};
+//    vector<int> a1 = {1,2,3};
+    string str = s.multiply("1000","1000");
+    std::cout << "Hello, World!\n";
+    return 0;
+}
