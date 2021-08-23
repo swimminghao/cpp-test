b9b73bf357c7180fd7f1f1e69888608ec9125686
xh <xuehao@shanzhen.com>
2021-08-23 22:50:29 +0800

    滑动谜题 commit

---
+++ leetcode-773/solution.cpp
@@ -0,0 +1,15 @@
+//
+// Created by xh on 2021/8/23.
+//
+
+#include "solution.h"
+#include <iostream>
+
+int main() {
+
+solution s;
+vector<vector<int>> a = {{1,2,3},{4,0,5}};
+std::cout << s.slidingPuzzle(a)<<endl;
+std::cout << "Hello, World!\n";
+return 0;
+}
