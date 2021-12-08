1ffad90218f8db72bbf0c5981c34622d53c8fd2e
xh <xuehao@shanzhen.com>
2021-12-08 09:39:28 +0800

    Changes

--- leetcode-886/solution.cpp
+++ leetcode-886/solution.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "solution.h"
-int main() {
+
+int main()
+{
     solution s;
-    vector<vector<int>> a = {{1,2},{1,3},{2,4}};
-    std::cout << s.possibleBipartition(4,a)<<endl;
+    vector<vector<int>> a = {{1, 2},
+                             {1, 3},
+                             {2, 4}};
+    std::cout << s.possibleBipartition(4, a) << endl;
     std::cout << "Hello, World!\n";
     return 0;
 }
